d10746472453feb6b5a56918576a4252924a9228
ThibautE <thibaut07600@gmail.com>
2017-03-15 12:58:58 +0100

    Modification bdd + changement couleur

--- app/src/main/assets/08_Browsing$Pois_2_Adding$Radar/js/poi.js
+++ app/src/main/assets/08_Browsing$Pois_2_Adding$Radar/js/poi.js
@@ -1,10 +1,13 @@
 var myJsonData = [
-                 {"id":"1","longitude":"3.863484","latitude":"43.631628","description":"Bat.7","name":"Administration"},
-                 {"id":"2","longitude":"3.864466","latitude":"43.631409","description":"Lundi-Vendredi : 8h-19h","name":"Bibliothèque Universitaire"},
-                 {"id":"3","longitude":"3.863382","latitude":"43.633077","description":"Cafétéria du campus","name":"(S)pace"},
-                 {"id":"4","longitude":"3.862454","latitude":"43.632731","description":"","name":"Polytech"},
-                 {"id":"5","longitude":"3.861551","latitude":"43.632455","description":"Institut d'Administration des Entreprises","name":"IAE"},
-                 {"id":"6","longitude":"3.862057","latitude":"43.633042","description":"Amphis et Salles de TP","name":"Batiment 5"},
-                 {"id":"7","longitude":"3.866890","latitude":"43.632212","description":"Geoscience (CNRS + UM)","name":"Batiment 22"},
-                 //{"id":"8","longitude":"43.640367","latitude":"3.845848","description":"Administration du département Informatique","name":"Batiment 16"}
+                 {"id":"2","longitude":"3.861753","latitude":"43.634046","description":"","name":"Bat. 2"},
+                 {"id":"3","longitude":"3.861351","latitude":"43.633895","description":"","name":"Bat. 3"},
+                 {"id":"4","longitude":"3.860846","latitude":"43.633888","description":"","name":"Bat. 4"},
+                 {"id":"5","longitude":"3.862057","latitude":"43.633042","description":"Amphis et Salles de TP","name":"Bat. 5"},
+                 {"id":"6","longitude":"3.863995","latitude":"43.632729","description":"Amphis et Salles de TP","name":"Bat. 6"},
+                 {"id":"7","longitude":"3.863484","latitude":"43.631628","description":"Bat.7","name":"Administration"},
+                 {"id":"8","longitude":"3.864466","latitude":"43.631409","description":"Lundi-Vendredi : 8h-19h","name":"Bibliothèque Universitaire"},
+                 {"id":"22","longitude":"3.866890","latitude":"43.632212","description":"Geoscience (CNRS + UM)","name":"Bat. 22"},
+                 {"id":"29","longitude":"3.861590","latitude":"43.632426","description":"Institut d'Administration des Entreprises","name":"IAE"},
+                 {"id":"31","longitude":"3.862454","latitude":"43.632731","description":"","name":"Polytech"},
+                 {"id":"38","longitude":"3.863382","latitude":"43.633077","description":"Cafétéria du campus","name":"(S)pace"},
                  ];
